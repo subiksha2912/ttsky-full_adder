d5c073a7b99ab60cac19ebfc2a2e9e4487d23eb4
Subiksha <subiksha29cbe@gmail.com>
2026-04-25 12:28:57 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,7 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
 
 import cocotb
 from cocotb.triggers import Timer
@@ -9,7 +11,6 @@ from cocotb.triggers import Timer
 async def test_full_adder(dut):
     dut._log.info("Starting Gate-level Hardware Simulation...")
 
-
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
@@ -17,7 +18,6 @@ async def test_full_adder(dut):
     await Timer(50, unit="ns")
     dut.rst_n.value = 1
     await Timer(50, unit="ns")
-
 
     test_cases = [
         (0, 0, 0, 0, 0),
@@ -27,22 +27,21 @@ async def test_full_adder(dut):
         (0, 1, 0, 1, 0),
     ]
 
-    # Set the input values you want to test
     for a, b, c, e_sum, e_carry in test_cases:
-        dut.ui_in.value = (c << 2) | (b << 1) | a 
+        dut.ui_in.value = (c << 2) | (b << 1) | a
         await Timer(20, unit="ns")
 
-    
-       try:
-           output_val = int(dut.uo_out.value)
-           actual_sum = output_val & 1
-           actual_carry = (output_val >> 1) & 1
+        try:
+            output_val = int(dut.uo_out.value)
+            actual_sum = output_val & 1
+            actual_carry = (output_val >> 1) & 1
 
+            assert actual_sum == e_sum, f"Sum Error: A={a} B={b} C={c}"
+            assert actual_carry == e_carry, f"Carry Error: A={a} B={b} C={c}"
+            dut._log.info(
+                f"Input: {a},{b},{c} -> Sum: {actual_sum}, Carry: {actual_carry} [PASS]"
+            )
 
-          assert actual_sum == e_sum, f"Sum Error: A={a} B={b} C={c}"
-          assert actual_carry == e_carry, f"Carry Error: A={a} B={b} C={c}"
-          dut._log.info(f"Input: {a},{b},{c} -> Sum: {actual_sum}, Carry: {actual_carry} [PASS]")
-
-      except ValueError:
-        dut._log.error(f"Logic error: uo_out is {str(dut.uo_out.value)}")
-        raise
+        except ValueError:
+            dut._log.error(f"Logic error: uo_out is {str(dut.uo_out.value)}")
+            raise
